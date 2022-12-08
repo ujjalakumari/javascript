f626a0683f4305049f8b4182dad1ae7c2b466f34
ujjala kumari <113276682+ujjalakumari@users.noreply.github.com>
2022-12-08 23:24:28 +0530

    Add files via upload

---
+++ java/main.java
@@ -0,0 +1,32 @@
+
+public class main {
+
+    public static void main(String[] args) {
+        // output
+        // System.out.print("smriti singh\nujjala");
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+        // variable
+        // String name = "smriti singh";
+        // int a = 25;
+        // int b = 30;
+        // double price = 25.25;n
+
+        // int a = 10;
+        // int b = 20;
+        // int sum = a + b;
+        // System.out.println(sum);
+        // int diff = b - a;
+        // System.out.println(diff);
+        // int mul = a * b;
+        // System.out.println(mul);
+        // // int ans = a * b / a - b;
+        // // System.out.println(ans);
+        // int ans = (a * b) / (a - b);
+        // System.out.println(ans);
+        // input
+
+    }
+}
